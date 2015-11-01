02798bc71a090be56e80b3e33312d0ddc8bc8562
GeorgeTG <george_gougoudis@hotmail.com>
2015-11-01 21:55:52 +0200

    added disclaimer

--- host/LivestreamerWrapper.py
+++ host/LivestreamerWrapper.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python2
+"""
+LivestreamerWrapper.py: A wrapper around the livestreamer programm
+Copyright (C) 2015 George T. Gougoudis <george_gougoudis@hotmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+#also check https://github.com/chrippa/livestreamer
+
 from multiprocessing import Process
 from threading import Thread, Event
 from subprocess import Popen
